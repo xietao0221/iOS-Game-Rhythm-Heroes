63fdfcb6027c02100426bc58642b1d5cf97abde2
Tao Xie <xie0221@126.com>
2016-05-31 17:27:57 -0700

    1

---
+++ Music/Music/Assets/planeControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeControl : MonoBehaviour {
+	public GameObject Sphere;
+	public sphereControl obj;
+	public float timeStart = -1;
+	public float timeLong = 100;
+	public float speed = 10;
+	public float timeNow = 10;
+
+	// Use this for initialization
+	void Start () {
+		Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		Sphere.AddComponent<Rigidbody>();
+		Sphere.transform.forward = this.transform.forward;
+		Sphere.transform.position = new Vector3(0, 2, 4);
+		//Sphere.transform.position = transform.position - transform.forward/speed;
+		obj = Sphere.GetComponent<sphereControl>();
+//		obj.timeStart = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Music/Music/Assets/sphereControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+// Awake() Start() 初始化
+// Instantiate 创建game object
+// Update() LateUpdate() FixedUpdate() 更新逻辑
+// GetComponment 找到其他的脚本
+// GameObject.find 找到其他的物品
+
+public class sphereControl : MonoBehaviour {
+
+	public GameObject Sphere;
+
+	// Use this for initialization
+	public float timeStart = -1;
+	public float timeLong = 100;
+	public float speed = 10;
+	public float timeNow = 10;
+
+	void Start () {
+		GameObject plane = GameObject.Find ("Plane");
+		this.transform.forward = plane.transform.forward;
+		timeStart = Time.time;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position - transform.forward/speed;
+		timeNow = Time.time;
+		if (Time.time-timeStart>timeLong) {
+			Debug.Log (Time.time);
+			Debug.Log ("->");
+			Debug.Log (timeStart);
+			Destroy (gameObject);
+		}
+	}
+}
