3ccd8dbef6a7f91eab0ce08a512f356df803141b
Tao Xie <xie0221@126.com>
2016-06-01 16:35:09 -0700

    0601_1634: each sphere can be controlled; queue is used to manage spheres.

--- Music/Music/Assets/PlaneController.cs
+++ Music/Music/Assets/PlaneController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlaneController : MonoBehaviour {
 
-	public GameObject prefabSphere;
+	public Queue<GameObject> queueSphere;
+	public GameObject prefabSphere, prefabPlane;
+	GameObject[] sphereClone = new GameObject[4];
+	MeshRenderer[] sphereRenderer = new MeshRenderer[4];
+	int[] speed = new int[]{50, 40, 30, 20};
 	public Vector3[] startingPoints;
-	int speed = 1;
+
 
 	// Use this for initialization
 	void Start () {
+		prefabPlane = GameObject.Find ("Plane");
+		queueSphere = new Queue<GameObject> ();
 		Collider collider = GetComponent<Collider> ();
 		Vector3 p1 = collider.bounds.min, p2 = collider.bounds.max;
 		float len = (float)((p2.x - p1.x) * 0.8);
@@ -17,23 +24,34 @@ public class PlaneController : MonoBehaviour {
 
 		startingPoints = new Vector3[4];
 		for(int i=0; i<4; i++) {
+			// position
 			startingPoints [i] = new Vector3 (p1.x + i * len / 3, p2.y, p2.z);
-			Instantiate (prefabSphere, startingPoints[i], Quaternion.identity);
+
+			// create obj
+			sphereClone[i] = Instantiate (prefabSphere, startingPoints[i], Quaternion.identity) as GameObject;
+
+			// Color
+			sphereRenderer[i] = sphereClone[i].gameObject.GetComponent<MeshRenderer> () as MeshRenderer;
+			if(i == 0) sphereRenderer[i].material.color = Color.red;
+			else if(i == 1) sphereRenderer[i].material.color = Color.black;
+			else if(i == 2) sphereRenderer[i].material.color = Color.green;
+			else sphereRenderer[i].material.color = Color.blue;
+
+			queueSphere.Enqueue (sphereClone[i]);
 		}
 
-		prefabSphere.transform.forward = this.transform.forward;
-		speed = 100;
+//		prefabSphere.transform.forward = this.transform.forward;
 		//		InvokeRepeating ("InitSphere", 1.0F, 1.0F);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		prefabSphere.transform.position += prefabSphere.transform.forward * Time.deltaTime * speed;;
-		//		print (this.);
-		//		prefabSphere.transform.Translate(Vector3.forward * Time.deltaTime);
+		for(int i=0; i<4; i++) {
+			sphereClone [i].transform.position -= prefabPlane.transform.forward / speed [i];
+		}
 	}
 
-	void InitSphere() {
-		Instantiate (prefabSphere, new Vector3(0, 5, 5), Quaternion.identity);
-	}
+//	void InitSphere() {
+//		Instantiate (prefabSphere, new Vector3(0, 5, 5), Quaternion.identity);
+//	}
 }
--- Music/Music/Assets/SphereController.cs
+++ Music/Music/Assets/SphereController.cs
@@ -2,21 +2,14 @@
 using System.Collections;
 
 public class SphereController : MonoBehaviour {
-
-	public MeshRenderer sphereRenderer;
-	GameObject planeObj;
-	int speedPara = 30;
-
+	
 	// Use this for initialization
 	void Start () {
-		sphereRenderer = gameObject.GetComponent<MeshRenderer>() as MeshRenderer;
-		sphereRenderer.material.color = Color.black;
-		planeObj = GameObject.Find ("Plane");
-		this.transform.forward = planeObj.transform.forward;
+		
 	}
 
 	// Update is called once per frame
 	void Update () {
-		this.transform.position -= this.transform.forward / speedPara;
+
 	}
 }
