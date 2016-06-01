f612284d8937f083a9e2af2394dabcc2c7d910d5
Tao Xie <xie0221@126.com>
2016-06-01 14:20:25 -0700

    0601: there are two controller script: plane and sphere; plane control the initiate of sphere, and sphere control its color and position.

--- Music/Music/Assets/PlaneController.cs
+++ Music/Music/Assets/PlaneController.cs
@@ -1,26 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class planeController: MonoBehaviour {
+public class PlaneController : MonoBehaviour {
 
-	public GameObject Sphere;
-
-	public float timeStart = -1;
-	public float timeLong = 100;
-	public float speed = 10;
-	public float timeNow = 10;
+	public GameObject prefabSphere;
+	public Vector3[] startingPoints;
+	int speed = 1;
 
 	// Use this for initialization
 	void Start () {
-		Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-		Sphere.AddComponent<Rigidbody>();
-		Sphere.transform.position = new Vector3(0, 1, 4);
-		Sphere.transform.forward = this.transform.forward;
+		Collider collider = GetComponent<Collider> ();
+		Vector3 p1 = collider.bounds.min, p2 = collider.bounds.max;
+		float len = (float)((p2.x - p1.x) * 0.8);
+		p1.x += len / 8;
+		p2.x -= len / 8;
+
+		startingPoints = new Vector3[4];
+		for(int i=0; i<4; i++) {
+			startingPoints [i] = new Vector3 (p1.x + i * len / 3, p2.y, p2.z);
+			Instantiate (prefabSphere, startingPoints[i], Quaternion.identity);
+		}
+
+		prefabSphere.transform.forward = this.transform.forward;
+		speed = 100;
+		//		InvokeRepeating ("InitSphere", 1.0F, 1.0F);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		prefabSphere.transform.position += prefabSphere.transform.forward * Time.deltaTime * speed;;
+		//		print (this.);
+		//		prefabSphere.transform.Translate(Vector3.forward * Time.deltaTime);
+	}
 
+	void InitSphere() {
+		Instantiate (prefabSphere, new Vector3(0, 5, 5), Quaternion.identity);
 	}
 }
---
+++ Music/Music/Assets/SphereController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+
+	public MeshRenderer sphereRenderer;
+	GameObject planeObj;
+	int speedPara = 30;
+
+	// Use this for initialization
+	void Start () {
+		sphereRenderer = gameObject.GetComponent<MeshRenderer>() as MeshRenderer;
+		sphereRenderer.material.color = Color.black;
+		planeObj = GameObject.Find ("Plane");
+		this.transform.forward = planeObj.transform.forward;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position -= this.transform.forward / speedPara;
+	}
+}
