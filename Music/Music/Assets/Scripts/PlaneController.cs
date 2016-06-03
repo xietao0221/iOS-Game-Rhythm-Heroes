9f07e4e7130c8bdb15f535deff36bec36532f259
Tao Xie <xie0221@126.com>
2016-06-02 17:31:38 -0700

    0602_1731

--- Music/Music/Assets/Scripts/PlaneController.cs
+++ Music/Music/Assets/Scripts/PlaneController.cs
@@ -5,28 +5,39 @@ using System.Collections.Generic;
 public class PlaneController : MonoBehaviour {
 
 	public static int blockNumPerChannel = 5;
-	public Queue<GameObject>[] queueBlocks = new Queue<GameObject>[4];
+	public Queue<GameObject>[] blocksInPool = new Queue<GameObject>[4], blocksInChannel = new Queue<GameObject>[4];
 	public GameObject prefabBlock;
-	public GameObject[] blockClone = new GameObject[blockNumPerChannel * 4];
-	public MeshRenderer[] rendererBlock = new MeshRenderer[4];
+	public GameObject[] blockClone = new GameObject[4 * blockNumPerChannel];
+	public MeshRenderer[] rendererBlock = new MeshRenderer[4 * blockNumPerChannel];
 
 	private Vector3[] startingPoints = new Vector3[4], endingPoints = new Vector3[4];
+//	private float 
 	private int[] speed = new int[]{50, 40, 30, 20};
 
 
 	void Awake() {
-		buildBlocksPool (blockNumPerChannel);
+		calculatePosition ();
+		initiateBlocks (blockNumPerChannel);
 	}
 		
 
 	void Start () {
-
+		//move four blocks from pool to channal
+		for(int i=0; i<4; i++) {
+			GameObject tmpBlock = blocksInPool [i].Dequeue();
+			blocksInChannel [i].Enqueue (tmpBlock);
+			tmpBlock.transform.position = new Vector3 (startingPoints[i].x, startingPoints[i].y, startingPoints[i].z);
+		}
 	}
 
 
 	void Update () {
+		// make all blocks move forward; move them back to pool if they are at the ending points
 		for(int i=0; i<4; i++) {
-			blockClone [i].transform.position -= this.transform.forward / speed [i];
+				foreach( GameObject tmpBlock in blocksInChannel[i]) {
+				tmpBlock.transform.position -= this.transform.forward / speed [i];
+
+			}
 		}
 
 		if (Input.anyKeyDown) {
@@ -41,19 +52,9 @@ public class PlaneController : MonoBehaviour {
 			}
 		}
 	}
+		
 
-
-//	void InitSphere() {
-//		Instantiate (prefabSphere, new Vector3(0, 5, 5), Quaternion.identity);
-//	}
-
-
-	void buildBlocksPool(int num) {
-		// build 4 queues for 4 channals
-		for(int i=0; i<4; i++) {
-			queueBlocks[i] = new Queue<GameObject> ();	
-		}
-
+	void calculatePosition() {
 		// calculate the x-coordinate of 4 channels
 		Collider collider = GetComponent<Collider> ();
 		Vector3 leftBottomPoint = collider.bounds.min, rightTopPoint = collider.bounds.max;
@@ -61,27 +62,50 @@ public class PlaneController : MonoBehaviour {
 		leftBottomPoint.x += len / 8;
 		rightTopPoint.x -= len / 8;
 
+		// calculate the starting position and ending position
 		for(int i=0; i<4; i++) {
-			// calculate position
 			startingPoints [i] = new Vector3 (leftBottomPoint.x + i * len / 3, rightTopPoint.y, rightTopPoint.z);
 			endingPoints [i] = new Vector3 (leftBottomPoint.x + i * len / 3, leftBottomPoint.y, leftBottomPoint.z);
+		}
+	}
 
-			// create obj
-			blockClone[i] = Instantiate (prefabBlock, startingPoints[i], Quaternion.identity) as GameObject;
+	void initiateBlocks(int num) {
+		// build 4 pools and 4 channels which contain corresponding blocks
+		for(int i=0; i<4; i++) {
+			blocksInPool[i] = new Queue<GameObject> ();	
+			blocksInChannel [i] = new Queue<GameObject> ();
+		}
+			
+		int tmpIndex = 0;
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<blockNumPerChannel; j++) {
 
-			// Color
-			rendererBlock[i] = blockClone[i].gameObject.GetComponent<MeshRenderer> () as MeshRenderer;
-			if(i == 0) rendererBlock[i].material.color = Color.red;
-			else if(i == 1) rendererBlock[i].material.color = Color.black;
-			else if(i == 2) rendererBlock[i].material.color = Color.green;
-			else rendererBlock[i].material.color = Color.blue;
+				// calculate the index
+				tmpIndex = i * blockNumPerChannel + j;
 
-			//			queueSphere.Enqueue (sphereClone[i]);
+				// initiate block
+				blockClone[tmpIndex] = Instantiate (prefabBlock, 
+					new Vector3(tmpIndex, 100, 0), Quaternion.identity) as GameObject;
+
+				// render the block
+//				rendererBlock[tmpIndex] = blockClone[tmpIndex].gameObject.GetComponent<MeshRenderer> () as MeshRenderer;
+//				if(i == 0) rendererBlock[tmpIndex].material.color = Color.red;
+//				else if(i == 1) rendererBlock[tmpIndex].material.color = Color.black;
+//				else if(i == 2) rendererBlock[tmpIndex].material.color = Color.green;
+//				else rendererBlock[tmpIndex].material.color = Color.blue;
+
+				blocksInPool[i].Enqueue (blockClone[tmpIndex]);
+			}
 		}
 	}
 
 	void generateBlocks(int channel) {
-		print (channel);
+		if(blocksInPool[channel].Count > 0) {
+			GameObject tmpBlock = blocksInPool [channel].Dequeue();
+			blocksInChannel [channel].Enqueue (tmpBlock);
+			tmpBlock.transform.position = new Vector3 (
+				startingPoints[channel].x, startingPoints[channel].y, startingPoints[channel].z);
+		}
 	}
 
 }
