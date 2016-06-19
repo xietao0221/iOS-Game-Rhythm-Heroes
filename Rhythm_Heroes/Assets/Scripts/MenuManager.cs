4df44657a7cf5f55063a51d0aeef647ae7108c2e
Tao Xie <taoxie@usc.edu>
2016-06-19 11:25:02 -0700

    0619_1124

--- Rhythm_Heroes/Assets/Scripts/MenuManager.cs
+++ Rhythm_Heroes/Assets/Scripts/MenuManager.cs
@@ -19,4 +19,19 @@ public class MenuManager : MonoBehaviour {
 	public void GoTo_MainScene() {
 		SceneManager.LoadScene ("MainScene");
 	}
+
+	public void GoTo_PlayAgain() {
+		int channelNum = PlaneController.channelNum;
+		switch(channelNum) {
+		case 3:
+			SceneManager.LoadScene ("GameScene_ThreeTrack");
+			break;
+		case 4:
+			SceneManager.LoadScene ("GameScene_FourTrack");
+			break;
+		case 5:
+			SceneManager.LoadScene ("GameScene_FiveTrack");
+			break;
+		}
+	}
 }
